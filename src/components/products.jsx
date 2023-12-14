66c527f62b6583013b9938866d6e129cbb0cd96f
1Papillon1 <83593078+1Papillon1@users.noreply.github.com>
2023-12-14 09:41:38 +0100

    implementing components for products page.

--- src/components/products.jsx
+++ src/components/products.jsx
@@ -1,5 +1,6 @@
 import React, { useState, useEffect } from "react";
 import { toJS } from 'mobx';
+import desktopImage from '../images/products/desktop/list-desktop.jpg';
 
 function Products({ store }) {
 
@@ -17,34 +18,38 @@ function Products({ store }) {
             setDesktopData((toJS(desktop)));
         })
     }
-    console.log(desktopData);
+    
 
     useEffect(() => {
         showData();
-    }, [])
+    }, [desktopData])
 
-    console.log(desktopData);
+    
     return (
         <div className='layout'>
-            {store.getData()}
-            {loading && (
-                <div className='flex'>
-                    <h2>Loading</h2>
-                </div>
-            )}
-
-            {desktopData.length > 0 && (
-                <div className='flex'>
-                    {desktopData.map(item => (
-                        <div className='flex__box'>
-                            <h2>{item.name}</h2>
-                            <h3>{item.price}</h3>
-                        </div>
-                    ))}
-                </div>
-            )}
+            <div className='panel'>
+                <h2 className='panel__title'>Search our Shop for High Quality Products</h2>
+                <h3 className='panel__subtitle'>Welcome to TechShop. We offer you the best 
+                electronic products for the best prices and discounts. Get discounts for every 
+                next product you buy.</h3>
+            </div>
+            
+            <div className='container'>
+                {desktopData.length > 0 && (
+                    <div className='flex flex--2'>
+                        {desktopData.map(item => (
+                            <div className='flex__box flex__box--list'>
+                                <img className='flex__box__image' src={desktopImage} alt="image did not load" srcset="" />
+                                <h2 className='flex__box__title'>{item.name}</h2>
+                                <h3 className='flex__box__subtitle'>€{item.price}</h3>
+                            </div>
+                        ))}
+                    </div>
+                )}
+            </div>
 
         </div>
+        
     )
 }
 
